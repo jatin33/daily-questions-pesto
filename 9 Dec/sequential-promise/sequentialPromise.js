0c4ec8c8ec628b4f4c8eb6385b2a0bfbe3175e3e
jatin33 <jatinpanjwani12@gmail.com>
2019-12-09 21:01:49 +0530

    Fixed sequential promise problem

--- 9 Dec/sequential-promise/sequentialPromise.js
+++ 9 Dec/sequential-promise/sequentialPromise.js
@@ -1,7 +1,7 @@
 
 function sequentialPromise(promises) {
   return new Promise((resolve) => {
-    const results = promises.reduce((acc, currentPromise) => acc.then(() => currentPromise()), Promise.resolve());
+    const results = promises.reduce((acc, currentPromise) => acc.then((val) => currentPromise(val)), Promise.resolve());
     resolve(results);
   });
 }
