74767de448d82c547c1c9073f38ba19e54dcc1f0
jatin33 <jatinpanjwani12@gmail.com>
2019-12-22 00:42:24 +0530

    Solved 1 question for day

---
+++ Dec-22/does-end-with/doesEndWith.js
@@ -0,0 +1,16 @@
+
+function doesEndWith(sublist, list) {
+  const listLength = list.length;
+  const sublistLength = sublist.length;
+  const remainingListLength = listLength - sublistLength;
+  if (Array.isArray(list) && Array.isArray(sublist)) {
+    list = list.join('');
+    sublist = sublist.join('');
+  }
+  const remainingList = list.substring(remainingListLength);
+  return remainingList === sublist;
+}
+
+export {
+  doesEndWith,
+};
